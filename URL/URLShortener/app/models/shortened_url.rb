e4e4a7b7f91ad01338c2760c3aa65662a383151d
Brad Trick <btrick@gmail.com>
2021-01-28 15:11:05 -0500

    Added ShortenedURL class and table

---
+++ URL/URLShortener/app/models/shortened_url.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: shortened_urls
+#
+#  id           :bigint           not null, primary key
+#  long_url     :string           not null
+#  short_url    :string           not null
+#  submitter_id :integer          not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+class ShortenedUrl < ApplicationRecord
+    validates :long_url, :short_url, :submitter_id, presence: true
+    
+    
+
+end
